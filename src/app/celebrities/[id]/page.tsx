b16a561b1d627c94391b1b1b26a36807e499583c
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2025-01-27 13:39:43 +0100

    update

--- src/app/celebrities/[id]/page.tsx
+++ src/app/celebrities/[id]/page.tsx
@@ -1,16 +1,12 @@
 "use client"
 import React, { useEffect, useState } from 'react';
-// import { Icon } from '@iconify/react';
 import Image from 'next/image';
-// import Link from 'next/link';
 import axios from 'axios';
 import { useParams } from 'next/navigation';
 import Fancard from '@/app/landing/components/Fancard';
 import LeftSidebar from '@/components/LeftBar';
 import CelebritiesList from '../components/Celebrities';
 import NavBar from '@/components/NavBar';
-
-// import { useSearchParams } from "next/navigation";
 
 interface Artist {
   _id: number;
@@ -20,19 +16,6 @@ interface Artist {
   para2: string;
   para3: string;
   hitSong: string;
-  // platforms: {
-  //   spotify?: string;
-  //   soundCloud?: string;
-  //   youtube?: string;
-  //   instagram?: string;
-  //   appleMusic?: string;
-  //   beatport?: string;
-  //   bandcamp?: string;
-  //   twitter?: string;
-  //   deezer?: string;
-  //   audiomack?: string;
-  //   twitch?: string;
-  // };
   text: string;
 }
 
@@ -64,17 +47,18 @@ const ArtistProfile: React.FC = () => {
     return <div className="text-[#fff]">Loading artist data...</div>;
   }
 
-
-
   return (
-    <div>
+    <div className="overflow-hidden lg:flex">
       <NavBar />
       <div className="flex-1 lg:block">
         <LeftSidebar />
       </div>
-      <div className='flex-[3] lg:max-w-[75%] ml-auto lg:pe-[5rem] relative'>
-        <div className='absolute -left-[250px] md:left-[300px] top-[940px] w-[506.15px] h-[506.15px] bg-gradient-to-r from-[#FF4081] to-[#18FFFF] rounded-full -z-50 blur-[80px] opacity-60'>
-
+      <div className='lg:max-w-[75%] m-auto flex-[4] md:pt-10 relative'>
+        <div className='md:absolute left-[800px] top-[80px] opacity-50'>
+          <div className='absolute -left-[200px] md:-left-[500px] top-[200px] w-[406.15px] h-[406.15px] bg-gradient-to-r from-[#FF4081] to-[#18FFFF] rounded-full -z-50 blur-[70px] md:blur-[150px]'>
+          </div>
+          <div className='absolute -right-[120px] md:-right-[280px] top-[50px] w-[406.15px] h-[406.15px] bg-gradient-to-r from-[#8d49cd] to-[#18FFFF] rounded-full -z-50 blur-[70px] md:blur-[150px]'>
+          </div>
         </div>
         <section key={artist._id} className='p-4 py-[2rem]'>
           <header className='flex justify-between items-center p-3 ps-[20px] rounded-[16px] bg-gradient-to-r from-[#FFFFFF14] via-[#FFFFFF06] to-[#FFFFFF14] border-[#ffffff32] border'>
@@ -101,31 +85,10 @@ const ArtistProfile: React.FC = () => {
             <h3 className='text-[14px] text-[#fff] font-medium leading-[24px]'>{artist.name}&apos;s Hit-</h3>
             <p className='text-[14px] text-[#FFFFFFA3] font-normal leading-[21px]'>{artist.hitSong}</p>
           </div>
-
-          {/* <div className='mt-2'>
-            <h3 className='text-[14px] text-[#fff] font-medium leading-[24px]'>Find {artist.name}</h3>
-            <ul className='grid grid-cols-3 lg:w-[60%] gap-2 my-3'>
-              {artist.platforms.spotify && <li className='text-[14px] text-[#191414] hover:text-[#1DB954] font-bold leading-[21px] p-2 bg-[#1DB954] hover:bg-[#191414] transition duration-400 ease-in-out rounded-md'><Link href={artist.platforms.spotify}>Spotify</Link></li>}
-
-              {artist.platforms.soundCloud && <li className='text-[14px] text-[#FF5500] hover:text-[#14171A] font-bold leading-[21px] p-2 bg-[#14171A] hover:bg-[#FF4F00] transition duration-400 ease-in-out rounded-md'><Link href={artist.platforms.soundCloud}>SoundCloud</Link></li>}
-
-              {artist.platforms.youtube && <li className='text-[14px] text-[#FF0000] hover:text-[#14171A] font-bold leading-[21px] p-2 bg-[#14171A] hover:bg-[#FF4B4B] transition duration-400 ease-in-out rounded-md'><Link href={artist.platforms.youtube}>YouTube</Link></li>}
-
-              {artist.platforms.instagram && <li className='text-[14px] text-[#E4405F] hover:text-[#14171A] font-bold leading-[21px] p-2 bg-[#14171A] hover:bg-[#F5A5B0] transition duration-400 ease-in-out rounded-md'><Link href={artist.platforms.instagram}>Instagram</Link></li>}
-
-              {artist.platforms.appleMusic && <li className='text-[14px] text-[#FA2B5C] hover:text-[#14171A] font-bold leading-[21px] p-2 bg-[#14171A] hover:bg-[#F57B92] transition duration-400 ease-in-out rounded-md'><Link href={artist.platforms.appleMusic}>Apple Music</Link></li>}
-
-              {artist.platforms.twitter && <li className='text-[14px] text-[#1DA1F2] hover:text-[#14171A] font-bold leading-[21px] p-2 bg-[#14171A] hover:bg-[#1A91D0] transition duration-400 ease-in-out rounded-md'><Link href={artist.platforms.twitter}>Twitter</Link></li>}
-
-            </ul>
-          </div> */}
         </section>
-
         <Fancard />
-
         <CelebritiesList />
       </div>
-
     </div>
   );
 };
